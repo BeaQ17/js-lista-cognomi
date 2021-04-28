0b6a4bb5fddc478f23cf4d6677ae86ed5c5fd925
BeaQ17 <beaq235@gmail.com>
2021-04-28 17:13:48 +0200

    fixed uppercase

--- assets/main.js
+++ assets/main.js
@@ -2,9 +2,14 @@
 var surname = prompt("Scrivi il tuo cognome:");
 
 //inseriscilo in un array con altri cognomi
-var others = ["Rossi", "Verdi", "Bianchi", "Neri"];
+var others = ["Rossi", "Verdi", "Bianchi", "Neri", "Almi"];
+surname = surname.charAt(0).toUpperCase() + surname.slice(1);
 others.push(surname);
+
 
 //stampa la lista in ordine alfabetico
 others.sort();
 console.log(others);
+
+//scrivi la posizione in cui si trova il cognome dell'utente nella lista
+//(partendo da 1)
